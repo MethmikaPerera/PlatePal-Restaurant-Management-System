bf85cf8640bffe9791affdc27f96f843b180f6b0
Bhathiya-learning <bhathiya971@gmail.com>
2024-10-26 14:01:37 +0530

    Bhathiya
    03

--- PlatePal-Restaurant-Management-System/src/Interfaces/Customer.java
+++ PlatePal-Restaurant-Management-System/src/Interfaces/Customer.java
@@ -26,19 +26,36 @@ public class Customer extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setPreferredSize(new java.awt.Dimension(767, 618));
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 824, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 517, Short.MAX_VALUE)
+        );
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 824, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 517, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
--- PlatePal-Restaurant-Management-System/src/Interfaces/HomeWindow.java
+++ PlatePal-Restaurant-Management-System/src/Interfaces/HomeWindow.java
@@ -6,6 +6,8 @@ package Interfaces;
 
 import com.formdev.flatlaf.FlatIntelliJLaf;
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Point;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
@@ -15,11 +17,46 @@ import javax.swing.SwingUtilities;
  */
 public class HomeWindow extends javax.swing.JFrame {
 
-    /**
-     * Creates new form HomeWindow
-     */
+    private int frameSize;
+    private Point framePosition;
     public HomeWindow() {
         initComponents();
+    }
+    
+    private void moveRight(){
+        Thread t = new Thread(
+                    () -> {
+                        for (int i = 200; i >= 80; i-=10) {
+                            jPanel2.setPreferredSize(new Dimension(i,jPanel2.getHeight()));
+                            SwingUtilities.updateComponentTreeUI(jPanel2);
+                            
+                            try {
+                                Thread.sleep(10);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+            
+            );
+    }
+    
+    private void moveLeft(){
+        Thread t = new Thread(
+                    () -> {
+                        for (int i = 80; i <= 200; i+=10) {
+                            jPanel2.setPreferredSize(new Dimension(i,jPanel2.getHeight()));
+                            SwingUtilities.updateComponentTreeUI(jPanel2);
+                            
+                            try {
+                                Thread.sleep(10);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+            
+            );
     }
 
     /**
@@ -36,6 +73,7 @@ public class HomeWindow extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
+        jToggleButton1 = new javax.swing.JToggleButton();
         jPanel2 = new javax.swing.JPanel();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
@@ -52,6 +90,11 @@ public class HomeWindow extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setUndecorated(true);
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowDeiconified(java.awt.event.WindowEvent evt) {
+                formWindowDeiconified(evt);
+            }
+        });
 
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
 
@@ -79,14 +122,25 @@ public class HomeWindow extends javax.swing.JFrame {
         jLabel4.setFont(new java.awt.Font("Helvetica Neue", 1, 30)); // NOI18N
         jLabel4.setText("App Name");
 
+        jToggleButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Resources/menu.png"))); // NOI18N
+        jToggleButton1.setBorder(null);
+        jToggleButton1.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
+        jToggleButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jToggleButton1ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
+                .addComponent(jToggleButton1)
+                .addGap(38, 38, 38)
                 .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 170, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 725, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 655, Short.MAX_VALUE)
                 .addComponent(jLabel3)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jLabel2)
@@ -99,9 +153,10 @@ public class HomeWindow extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(7, 7, 7)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, 40, Short.MAX_VALUE)
+                    .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jToggleButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jLabel3)
                             .addComponent(jLabel2)
                             .addComponent(jLabel1))
@@ -117,7 +172,10 @@ public class HomeWindow extends javax.swing.JFrame {
         jPanel2.setLayout(new java.awt.GridLayout(12, 1, 0, 2));
 
         jButton1.setBackground(new java.awt.Color(233, 231, 231));
-        jButton1.setText("jButton1");
+        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Resources/user.png"))); // NOI18N
+        jButton1.setText("Customer");
+        jButton1.setHorizontalTextPosition(javax.swing.SwingConstants.LEFT);
+        jButton1.setIconTextGap(10);
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
@@ -126,43 +184,43 @@ public class HomeWindow extends javax.swing.JFrame {
         jPanel2.add(jButton1);
 
         jButton2.setBackground(new java.awt.Color(233, 231, 231));
-        jButton2.setText("jButton2");
+        jButton2.setText("Product");
         jPanel2.add(jButton2);
 
         jButton3.setBackground(new java.awt.Color(233, 231, 231));
-        jButton3.setText("jButton3");
+        jButton3.setText("Sales");
         jPanel2.add(jButton3);
 
         jButton4.setBackground(new java.awt.Color(233, 231, 231));
-        jButton4.setText("jButton4");
+        jButton4.setText("Attendance");
         jPanel2.add(jButton4);
 
         jButton5.setBackground(new java.awt.Color(233, 231, 231));
-        jButton5.setText("jButton5");
+        jButton5.setText("Stock");
         jPanel2.add(jButton5);
 
         jButton6.setBackground(new java.awt.Color(233, 231, 231));
-        jButton6.setText("jButton6");
+        jButton6.setText("Inventory");
         jPanel2.add(jButton6);
 
         jButton7.setBackground(new java.awt.Color(233, 231, 231));
-        jButton7.setText("jButton7");
+        jButton7.setText("Backup");
         jPanel2.add(jButton7);
 
         jButton8.setBackground(new java.awt.Color(233, 231, 231));
-        jButton8.setText("jButton8");
+        jButton8.setText("GRN");
         jPanel2.add(jButton8);
 
         jButton9.setBackground(new java.awt.Color(233, 231, 231));
-        jButton9.setText("jButton9");
+        jButton9.setText("Return");
         jPanel2.add(jButton9);
 
         jButton10.setBackground(new java.awt.Color(233, 231, 231));
-        jButton10.setText("jButton10");
+        jButton10.setText("PO");
         jPanel2.add(jButton10);
 
         jButton11.setBackground(new java.awt.Color(233, 231, 231));
-        jButton11.setText("jButton11");
+        jButton11.setText("Report");
         jPanel2.add(jButton11);
 
         getContentPane().add(jPanel2, java.awt.BorderLayout.LINE_START);
@@ -177,7 +235,7 @@ public class HomeWindow extends javax.swing.JFrame {
         );
         jPanel3Layout.setVerticalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 618, Short.MAX_VALUE)
+            .addGap(0, 569, Short.MAX_VALUE)
         );
 
         getContentPane().add(jPanel3, java.awt.BorderLayout.CENTER);
@@ -198,6 +256,8 @@ public class HomeWindow extends javax.swing.JFrame {
     }//GEN-LAST:event_jLabel1MouseClicked
 
     private void jLabel3MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel3MouseClicked
+        frameSize = this.getExtendedState();
+        framePosition = this.getLocation();
         this.setExtendedState(JFrame.ICONIFIED);
     }//GEN-LAST:event_jLabel3MouseClicked
 
@@ -209,6 +269,19 @@ public class HomeWindow extends javax.swing.JFrame {
         }
         
     }//GEN-LAST:event_jLabel2MouseClicked
+
+    private void formWindowDeiconified(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowDeiconified
+        this.setExtendedState(frameSize);
+        this.setLocation(framePosition);
+    }//GEN-LAST:event_formWindowDeiconified
+
+    private void jToggleButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jToggleButton1ActionPerformed
+        if (jToggleButton1.isSelected()) {
+            moveRight();
+        }else{
+            moveLeft();
+        }
+    }//GEN-LAST:event_jToggleButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -244,5 +317,6 @@ public class HomeWindow extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
+    private javax.swing.JToggleButton jToggleButton1;
     // End of variables declaration//GEN-END:variables
 }
